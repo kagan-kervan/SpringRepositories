9f574ae0a61774b04596a9096e43a73951ae3854
kagan-kervan <ahmetkagankervan@gmail.com>
2024-06-28 10:29:53 +0300

    Initial project commit

---
+++ TShirtMicroServices/DataAcquireService/src/test/java/com/example/DataAcquireService/DataAcquireServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataAcquireService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAcquireServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
